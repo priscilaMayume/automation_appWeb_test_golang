248323d04cbb905a0240335df0da8afc1318a00b
priscilaMayume <prisccila@gmail.com>
2024-05-27 18:16:02 -0300

    unit tests status code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates/"
 
 // Função para lidar com a rota Home
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -17,14 +20,14 @@ type TemplateData struct {
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
-	// parse the template from disc
-	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+		// analisar o modelo a partir do disco
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return err
 	}
 
-	// execute the template, passing it data, if any
+	// executa o modelo, passando-lhe dados, se existirem
 	err = parsedTemplate.Execute(w, data)
 	if err != nil {
 		return err
